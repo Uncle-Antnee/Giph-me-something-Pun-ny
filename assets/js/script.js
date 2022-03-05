d91e4e6081c0e1fff55cb3deeda6b992a205632e
Alex Haas <alexzhaas@gmail.com>
2022-03-04 19:32:39 -0700

    beginnings of local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,8 @@
-java
+let favorites = document.getElementById("favorite");
+console.log(favorites)
+
+localStorage.setItem("temp", favorites)
+
+localStorage.getItem("temp").value = favorites;
+
+document.getElementById("favorite-tab") = favorites;
