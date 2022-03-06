43e80c4882a64978188ebdcaf7de2b4b6b4d43c6
Alex Haas <alexzhaas@gmail.com>
2022-03-06 00:28:49 -0700

    minor local storage changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-
+// START OF LOCAL STORAGE
 let favorites = document.getElementById("favorite");
 console.log(favorites)
 
@@ -7,9 +7,11 @@ localStorage.setItem("temp", favorites)
 localStorage.getItem("temp").value = favorites;
 
 document.getElementById("favorite-tab") = favorites;
+//END OF LOCAL STORAGE
 
 // START OF GIPHY API
 document.addEventListener("DOMContentLoaded", init);
+
 function init() {
     document.getElementById("btnSearch").addEventListener("click", ev => {
         ev.preventDefault()
@@ -18,25 +20,25 @@ function init() {
         url = url.concat(str);
         console.log(url);
         fetch(url)
-        .then(response => response.json())
-        .then(content => {
-            console.log(content.data);
-            console.log("META", content.meta);
-            let fig = document.createElement("figure");
-            let img = document.createElement("img");
-            let fc = document.createElement("figcaption");
-            img.src = content.data[0].images.downsized.url;
-            img.alt = content.data[0].title;
-            fc.textContent = content.data[0].title;
-            fig.appendChild(img);
-            fig.appendChild(fc);
-            let giph = document.querySelector(".giph");
-            giph.insertAdjacentElement("afterbegin", fig);
-            document.querySelector("#search").value = "";
-        })
-        .catch(err => {
-            console.error(err);
-        });
+            .then(response => response.json())
+            .then(content => {
+                console.log(content.data);
+                console.log("META", content.meta);
+                let fig = document.createElement("figure");
+                let img = document.createElement("img");
+                let fc = document.createElement("figcaption");
+                img.src = content.data[0].images.downsized.url;
+                img.alt = content.data[0].title;
+                fc.textContent = content.data[0].title;
+                fig.appendChild(img);
+                fig.appendChild(fc);
+                let giph = document.querySelector(".giph");
+                giph.insertAdjacentElement("afterbegin", fig);
+                document.querySelector("#search").value = "";
+            })
+            .catch(err => {
+                console.error(err);
+            });
     });
 }
 //END OF GIHPY API
